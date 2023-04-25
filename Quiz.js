265e8c2068a002a71724132b2608e1d13c8ec3ff
Mario Mitic <mariomitic@yahoo.com>
2023-04-25 09:29:21 -0700

    removed test files

--- Quiz.js
+++ Quiz.js
@@ -7,18 +7,22 @@ async function nextQuestion() {
     clickCount = clickCount + 1;
     event.preventDefault();
 
-    const response = await fetch('./Test.json');
-    const mesta = await response.json();
-    let current = mesta[clickCount];
-    let last = mesta[clickCount - 1]
+    const response = await fetch('https://planinarske-akcije.com/quiz');
+    const places = await response.json();
+    let current = places[clickCount];
+     
+    let last = places[clickCount - 1]
+    //console.log("Last Id: " + last.id)
+
+     console.log(current.mountainOnPicture)
+    // console.log(typeof(clickCount))
    
-      
-    if(clickCount === 9){
-        if(document.getElementById('options1').checked && last.options.option1.correct) {
+    if(clickCount === 10){
+        if(document.getElementById('radio1').checked && last.options.option1.correct) {
             correctAnswers = correctAnswers + 1}
-            else if(document.getElementById('options2').checked && last.options.option2.correct) {
+            else if(document.getElementById('radio2').checked && last.options.option2.correct) {
                 correctAnswers = correctAnswers + 1}
-                else if(document.getElementById('options3').checked && last.options.option3.correct) {
+                else if(document.getElementById('radio3').checked && last.options.option3.correct) {
                     correctAnswers = correctAnswers + 1}
 
         document.getElementById("next").disabled = true;
@@ -28,23 +32,24 @@ async function nextQuestion() {
         var resultViz = document.getElementById("result");
         resultViz.classList.remove("result");
         clickCount = clickCount - 1;
+
         return}
 
 
     
     document.getElementById("picture").src = current.picture;
-    document.getElementById("title").innerHTML = current.title;
+    document.getElementById("title").innerHTML = current.mountainOnPicture;
     document.getElementById("options1").innerHTML = current.options.option1.picture;
-    document.getElementById("options2").innerHTML = current.options.option2;
-    document.getElementById("options3").innerHTML = current.options.option3;
+    document.getElementById("options2").innerHTML = current.options.option2.picture;
+    document.getElementById("options3").innerHTML = current.options.option3.picture;
 
-    if(document.getElementById('options1').checked && current.options.option1.correct) {
+    if(document.getElementById('radio1').checked && current.options.option1.correct) {
         correctAnswers = correctAnswers + 1}
-        else if(document.getElementById('options2').checked && current.options.option2.correct) {
+        else if(document.getElementById('radio2').checked && current.options.option2.correct) {
             correctAnswers = correctAnswers + 1}
-            else if(document.getElementById('options3').checked && current.options.option3.correct) {
+            else if(document.getElementById('radio3').checked && current.options.option3.correct) {
                 correctAnswers = correctAnswers + 1}
-    console.log(correctAnswers)
+   // console.log(correctAnswers)
    
     if(clickCount === 0){
         document.getElementById("previous").disabled = true;
@@ -57,24 +62,24 @@ async function prevQuestion() {
     
     event.preventDefault();
     document.getElementById("result").disabled = true;
-    const response = await fetch('./Test.json');
-    const mesta = await response.json();
+    const response = await fetch('https://planinarske-akcije.com/quiz');
+    const places = await response.json();
 
 
    
-    let current = mesta[clickCount];
+    let current = places[clickCount];
     document.getElementById("next").disabled = false;
     document.getElementById("picture").src = current.picture;
     document.getElementById("title").innerHTML = current.title;
-    document.getElementById("options1").innerHTML = current.options.option1;
-    document.getElementById("options2").innerHTML = current.options.option2;
-    document.getElementById("options3").innerHTML = current.options.option3;
+    document.getElementById("options1").innerHTML = current.options.option1.picture;
+    document.getElementById("options2").innerHTML = current.options.option2.picture;
+    document.getElementById("options3").innerHTML = current.options.option3.picture;
 
-    if(document.getElementById('options1').checked && current.options.option1.correct) {
+    if(document.getElementById('radio1').checked && current.options.option1.correct) {
         correctAnswers = correctAnswers - 1}
-        else if(document.getElementById('options2').checked && current.options.option2.correct) {
+        else if(document.getElementById('radio2').checked && current.options.option2.correct) {
             correctAnswers = correctAnswers - 1}
-            else if(document.getElementById('options3').checked && current.options.option3.correct) {
+            else if(document.getElementById('radio3').checked && current.options.option3.correct) {
                 correctAnswers = correctAnswers - 1}
     //console.log(correctAnswers)
 
@@ -84,7 +89,6 @@ async function prevQuestion() {
         document.getElementById("previous").disabled = true;
         return}
 }
-
 function startQuiz() {
     var startBtn = document.getElementById("start");
     startBtn.classList.add("start");
@@ -94,6 +98,10 @@ function startQuiz() {
     formViz.classList.remove("form");
     nextQuestion()
 
+   
+    // fetch('https://planinarske-akcije.com/api/v1/docs/gateway#/Fun/get_quiz_quiz_get').
+    // then(resp => {return resp.json();}).
+    // then(data => {console.log(data)}).catch(err => console.log(err));
 }
 
 function showResults() {
@@ -111,5 +119,4 @@ document.getElementById('next').addEventListener('click', nextQuestion, false);
 document.getElementById('previous').addEventListener('click', prevQuestion, false);
 document.getElementById('start').addEventListener('click', startQuiz, false);
 document.getElementById('result').addEventListener('click', showResults, false);
-
 
